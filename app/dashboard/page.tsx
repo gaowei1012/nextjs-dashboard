6337df8bb520c354b16d6de14c734814dcc452e2
执念 <gyb2020018@163.com>
2024-03-27 16:40:25 +0800

    feat: 新增图表展示

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <p>dashboard</p>
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+
+export default async function Page() {
+
+  const revenue = await fetchRevenue()
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} text-1 md:text-2x1 mb-4`}>
+        Dashborad
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title='Collected' value={}/> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}/>
+      </div>
+    </main>
+  );
 }
